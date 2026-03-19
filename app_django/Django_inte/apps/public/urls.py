ebf5842dc55d3e4163b235f1e23aeb7e1ae93194
AleXCBRX12 <bolanosdelacruz.miltonalejandro@utacapulco.edu.mx>
2026-03-18 19:12:38 -0600

    Fix login back link to visitor portal

--- app_django/Django_inte/apps/public/urls.py
+++ app_django/Django_inte/apps/public/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.portal_visitante, name='portal_visitante'),
+    # Alias para evitar conflictos de reverse() con otras apps que también usan "portal_visitante"
+    path('inicio/', views.portal_visitante, name='portal_visitante_public'),
     path('portal_publico/', views.portal_publico, name='portal_publico'),
     path('login/', views.login_view, name='login'),
     path('login/forgot/', views.solicitar_reset, name='solicitar_reset'),
